0a2888f93241d777ee935c3af250f52b3b56ee8f
Owen Law <owenlaw222@gmail.com>
2024-12-10 00:52:47 -0500

    Day 10 part 2

--- day_10/src/main.rs
+++ day_10/src/main.rs
@@ -1,20 +1,25 @@
-use std::collections::HashSet;
+use std::collections::HashMap;
 
 fn main() {
     let input = load_input();
     let width = input.first().unwrap().len();
     let height = input.len();
 
-    let mut sum = 0;
+    let mut p1_sum = 0;
+    let mut p2_sum = 0;
+
     for (row_idx, row) in input.iter().enumerate() {
         for (col_idx, cell) in row.iter().enumerate() {
             if *cell == 0 {
-                sum += find_trails((row_idx, col_idx), &input, width, height);
+                let (p1, p2) = find_trails((row_idx, col_idx), &input, width, height);
+                p1_sum += p1;
+                p2_sum += p2;
             }
         }
     }
 
-    println!("{sum}");
+    println!("Part 1: {p1_sum}");
+    println!("Part 2: {p2_sum}");
 }
 
 fn load_input() -> Vec<Vec<u8>> {
@@ -36,34 +41,59 @@ fn find_trails(
     topographic_map: &[Vec<u8>],
     width: usize,
     height: usize,
-) -> usize {
-    let mut active_positions = HashSet::new();
-    let mut next_positions = HashSet::new();
+) -> (usize, usize) {
+    let mut active_positions = HashMap::new();
+    let mut next_positions = HashMap::new();
 
-    active_positions.insert((trail_head.0, trail_head.1));
+    active_positions.insert((trail_head.0, trail_head.1), 1);
 
     for elevation in 1..=9 {
-        // println!("searching elevation {elevation}, active: {active_positions:?}");
-        for (row, col) in &active_positions {
+        for ((row, col), paths) in &active_positions {
             if *row != 0 && topographic_map[row - 1][*col] == elevation {
-                next_positions.insert((row - 1, *col));
+                next_positions
+                    .entry((row - 1, *col))
+                    .and_modify(|next_paths| {
+                        *next_paths += *paths;
+                    })
+                    .or_insert(*paths);
             }
 
             if *row != width - 1 && topographic_map[row + 1][*col] == elevation {
-                next_positions.insert((row + 1, *col));
+                next_positions
+                    .entry((row + 1, *col))
+                    .and_modify(|next_paths| {
+                        *next_paths += *paths;
+                    })
+                    .or_insert(*paths);
             }
 
             if *col != 0 && topographic_map[*row][col - 1] == elevation {
-                next_positions.insert((*row, col - 1));
+                next_positions
+                    .entry((*row, col - 1))
+                    .and_modify(|next_paths| {
+                        *next_paths += *paths;
+                    })
+                    .or_insert(*paths);
             }
 
             if *col != height - 1 && topographic_map[*row][col + 1] == elevation {
-                next_positions.insert((*row, col + 1));
+                next_positions
+                    .entry((*row, col + 1))
+                    .and_modify(|next_paths| {
+                        *next_paths += *paths;
+                    })
+                    .or_insert(*paths);
             }
         }
 
         active_positions = std::mem::take(&mut next_positions);
     }
 
-    active_positions.len()
+    (
+        active_positions.len(),
+        active_positions
+            .iter()
+            .map(|(_, paths)| paths)
+            .sum::<usize>(),
+    )
 }
